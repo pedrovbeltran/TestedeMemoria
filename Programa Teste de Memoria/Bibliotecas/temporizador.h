7b8828d2a3157a774d59941d41a87213056fb551
Pedro Beltran <pepeu.beltran@gmail.com>
2018-03-24 11:14:30 -0300

    Create temporizador.h

---
+++ Programa Teste de Memoria/Bibliotecas/temporizador.h
@@ -0,0 +1,13 @@
+void pausa (float delay1) {
+
+   if (delay1<0.001) return; // pode ser ajustado e/ou evita-se valores negativos.
+
+   float inst1=0, inst2=0;
+
+   inst1 = (float)clock()/(float)CLOCKS_PER_SEC;
+
+   while (inst2-inst1<delay1) inst2 = (float)clock()/(float)CLOCKS_PER_SEC;
+
+   return;
+
+}
