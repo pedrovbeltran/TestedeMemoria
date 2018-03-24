b90b442a45fdf667f9de32e308006bc6bb91399e
Pedro Beltran <pepeu.beltran@gmail.com>
2018-03-24 11:08:34 -0300

    Create apresentacao.h

---
+++ Programa Teste de Memoria/Bibliotecas/apresentacao.h
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+void apresentacao()
+{
+	printf("Bem vindo ao Programa teste de memória\n");
+	printf("\nEste programa é um jogo para testar suas habilidades\n");
+	printf("\nBasta você decorar alguns números que aparecerão na tela \ne depois verificar se você observou este número\n");
+	printf("\nEntão vamos lá\n\n\n");
+	system("pause");
+	system("cls");
+}
